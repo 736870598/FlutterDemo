d0f01287277c20cf1a4d25c70bcfa789184c88b0
sunxy <sunxy@staff.tianya.cn>
2018-07-24 17:54:01 +0800

    .

---
+++ lib/Flutter_2.dart
@@ -0,0 +1,40 @@
+//https://blog.csdn.net/hekaiyou/article/details/52999238
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new TutorialHome(),
+  ));
+//  runApp
+}
+
+class TutorialHome extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: "导航菜单",
+            onPressed: null),
+        title: new Text("实例标题"),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.search),
+              tooltip: "search",
+              onPressed: null)
+        ],
+      ),
+      body: new Center(
+        child: new Text("nihao,nihao"),
+
+      ),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: "增加",
+          child: new Icon(Icons.add),
+          onPressed: null),
+    );
+  }
+
+}
---
+++ lib/Flutter_3.dart
@@ -0,0 +1,2 @@
+//https://blog.csdn.net/hekaiyou/article/details/53019717
+import 'package:flutter/material.dart';
