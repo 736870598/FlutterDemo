bc012a1e293dd2e606b35b29d2752185a112cd74
sunxy <sunxy@staff.tianya.cn>
2018-07-26 18:26:14 +0800

    .继续学习flutter

--- lib/Flutter_19.dart
+++ lib/Flutter_19.dart
@@ -12,8 +12,8 @@ class NavigationIconView{
       : _icon = icon,
         _color = color,
         item = new BottomNavigationBarItem(icon: icon, title: title),
-        controller =
-        new AnimationController(duration: kThemeAnimationDuration, vsync: vsync){
+
+        controller =  new AnimationController(duration: kThemeAnimationDuration, vsync: vsync){
     _animation = new CurvedAnimation(
       parent: controller,
       curve: new Interval(0.5,1.0,curve: Curves.fastOutSlowIn),
@@ -44,7 +44,32 @@ class NavigationIconView{
       ),
     );
   }
-
-
 }
 
+class CustomIcon extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final IconThemeData iconTheme = IconTheme.of(context);
+    return new Container(
+      margin: const EdgeInsets.all(4.0),
+      width: iconTheme.size - 8.0,
+      height: iconTheme.size - 8,
+      decoration: new BoxDecoration(
+          color: iconTheme.color
+      ),
+    );
+  }
+}
+
+class MenusDemo extends StatefulWidget{
+  @override
+  _MenusDemoState createState() => new _MenusDemoState();
+}
+
+
+class _MenusDemoState extends State<MenusDemo> with TickerProviderStateMixin{
+  @override
+  build(dynamic ) {
+  }
+
+}
