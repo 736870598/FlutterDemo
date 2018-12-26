60894993192f889c93469c87cd5552dcb7fb50fe
sunxy <736870598@qq.com>
2018-12-26 23:21:02 +0800

    继续学习flutter中...

--- lib/Flutter_33.dart
+++ lib/Flutter_33.dart
@@ -6,31 +6,49 @@ class MyGridView extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    List<Container> _buildGridTileList(int count){
-//      return new List<Container>.generate(length, generator)
-      return new List<Container>.generate(
-          count, (int index){new Container(child: new Image.asset("images/img_${(index % 4) + 1}.jpg"));}
+
+    List<Image> _buildGridTileList(int count){
+      return new List<Image>.generate(
+          count,
+              (index){new Image.asset("images/img_1.jpg");}
       );
     }
 
     Widget buildGrid(){
-      return new GridView.extent(
-        maxCrossAxisExtent: 150,
+      return new GridView.count(
+        shrinkWrap: true,
         padding: const EdgeInsets.all(4.0),
         mainAxisSpacing: 4.0,
         crossAxisSpacing: 4.0,
+        crossAxisCount: 2,
         children: _buildGridTileList(20),
+//        children: <Widget>[
+//          new Image.asset('images/img_1.jpg'),
+//          new Image.asset('images/img_2.jpg'),
+//          new Image.asset('images/img_3.jpg'),
+//          new Image.asset('images/img_4.jpg'),
+//          new Image.asset('images/img_1.jpg'),
+//          new Image.asset('images/img_2.jpg'),
+//          new Image.asset('images/img_3.jpg'),
+//          new Image.asset('images/img_4.jpg'),
+//          new Image.asset('images/img_1.jpg'),
+//          new Image.asset('images/img_2.jpg'),
+//          new Image.asset('images/img_3.jpg'),
+//          new Image.asset('images/img_4.jpg'),
+//        ],
       );
     }
 
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("------"),
+        title: new Text("GridView（网格视图）"),
       ),
       body: new Center(
-        child: buildGrid(),
+        child: buildGrid() ,
       ),
     );
+
+//    return buildGrid();
   }
 
 }
